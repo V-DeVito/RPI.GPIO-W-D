ac2751f28dc6ec8a9c3715940d2323c0b258a8d2
Enzo <enzo-code@users.noreply.github.com>
2017-08-29 17:30:03 -0400

    Update wfinal.py

--- wfinal.py
+++ wfinal.py
@@ -10,7 +10,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(channels, GPIO.OUT)
 GPIO.output(channels, 0)
 
-weather = pywapi.get_weather_from_weather_com('33020')
+weather = pywapi.get_weather_from_weather_com('ZIP code')
 temperature = int(weather['current_conditions']['temperature'])
 temp_f = round(temperature*(1.8)+32)
 humidity = round(int(weather['current_conditions']['humidity']))
